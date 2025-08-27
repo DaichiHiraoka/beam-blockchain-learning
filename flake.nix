ba9dea7cb600d9c674d51b8187e03b4934dc8676
Daichi Hiraoka <135154711+DaichiHiraoka@users.noreply.github.com>
2025-08-27 20:02:33 +0900

    Refine dev shell description and inputs

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "BEAM + Rust dev shell for blockchain learning";
+  description = "BEAM + Rust dev shell (minimal to boot)";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
@@ -8,36 +8,20 @@
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
+      let pkgs = import nixpkgs { inherit system; };
       in {
         devShells.default = pkgs.mkShell {
           buildInputs = [
-            pkgs.erlang
-            pkgs.elixir
-            pkgs.rebar3
-            pkgs.rustc
-            pkgs.cargo
-            pkgs.rustfmt
-            pkgs.clippy
-            pkgs.gcc
-            pkgs.openssl
-            pkgs.zlib
-            pkgs.libsodium
-            pkgs.protobuf
-            pkgs.python3
-            pkgs.python3Packages.pip
-            pkgs.git
-            pkgs.curl
-            pkgs.jq
+            pkgs.erlang pkgs.elixir pkgs.rebar3
+            pkgs.rustc pkgs.cargo pkgs.rustfmt pkgs.clippy
+            pkgs.gcc pkgs.openssl pkgs.libsodium pkgs.protobuf
+            pkks.python3 or pkgs.python3
+            pkgs.git pkgs.curl pkgs.jq
           ];
           shellHook = ''
             export ERL_AFLAGS="-kernel shell_history enabled"
             export MIX_ENV=dev
             export RUST_BACKTRACE=1
-            echo "[DevShell Ready]"
-            elixir -v
-            rustc --version
           '';
         };
       });
