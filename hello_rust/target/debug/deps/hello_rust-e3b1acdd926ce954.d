e168c6b1f44463adfec8acc7f68e8d7d58347530
Daichi Hiraoka <135154711+DaichiHiraoka@users.noreply.github.com>
2025-08-27 18:13:38 +0000

    Add initial implementation of HelloBeam application with Nix and Rust support

---
+++ hello_rust/target/debug/deps/hello_rust-e3b1acdd926ce954.d
@@ -0,0 +1,5 @@
+/workspaces/beam-blockchain-learning/hello_rust/target/debug/deps/hello_rust-e3b1acdd926ce954: src/main.rs
+
+/workspaces/beam-blockchain-learning/hello_rust/target/debug/deps/hello_rust-e3b1acdd926ce954.d: src/main.rs
+
+src/main.rs:
---
+++ hello_rust/target/debug/hello_rust.d
@@ -0,0 +1,1 @@
+/workspaces/beam-blockchain-learning/hello_rust/target/debug/hello_rust: /workspaces/beam-blockchain-learning/hello_rust/src/main.rs
